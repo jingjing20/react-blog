ed450b61154cb016c941ab26822701d5d92a8cbe
jingjing20 <1269928993@qq.com>
2020-05-01 23:15:45 +0800

    前台项目初始化

---
+++ blog-client/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import { Button } from 'antd'
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <Button>jing</Button>
+    </>
+  )
+}
