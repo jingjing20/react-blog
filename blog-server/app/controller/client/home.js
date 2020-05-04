0a53f857f04070d42c70f7d952afb38534a1d66b
jingjing20 <1269928993@qq.com>
2020-05-04 12:03:50 +0800

    jing

--- blog-server/app/controller/client/home.js
+++ blog-server/app/controller/client/home.js
@@ -10,13 +10,14 @@ class HomeController extends Controller {
     const sql = 'SELECT article.id as id,' +
       'article.title as title,' +
       'article.introduce as introduce,' +
-      'article.addTime as addTime,' +
+      // 'article.addTime as addTime,' +
+      // "FROM_UNIXTIME(article.addTime,'%Y-%m-%d %H:%i:%s' ) as addTime," +
+      "date_format(article.addTime,'%Y-%m-%d %H:%i:%s' ) as addTime," +
       'article.view_count as view_count ,' +
-      '.type.typeName as typeName ' +
+      'type.typeName as typeName ' +
       'FROM article LEFT JOIN type ON article.type_id = type.id';
 
     const results = await this.app.mysql.query(sql);
-
     this.ctx.body = {
       data: results,
     };
