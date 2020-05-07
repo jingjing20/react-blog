0c1a19e090d86ddbcdc2719147b145e5e07269a7
jingjing20 <1269928993@qq.com>
2020-05-07 15:06:08 +0800

    加油干！

--- blog-admin/src/pages/Login.js
+++ blog-admin/src/pages/Login.js
@@ -1,46 +1,53 @@
 import React, { useState } from 'react';
 import 'antd/dist/antd.css';
-import { Card, Input, Button, Spin } from 'antd';
+import { Card, Input, Button, Spin, message } from 'antd';
 import { UserOutlined, KeyOutlined } from '@ant-design/icons';
 import '../static/css/Login.css';
-function Login() {
+import servicePath from '../config/apiUrl';
+import axios from 'axios';
+
+function Login(props) {
   const [userName, setUserName] = useState('')
   const [password, setPassword] = useState('')
   const [isLoading, setIsLoading] = useState(false)
   const checkLogin = () => {
     setIsLoading(true)
 
-    // if (!userName) {
-    //   message.error('用户名不能为空')
-    //   return false
-    // } else if (!password) {
-    //   message.error('密码不能为空')
-    //   return false
-    // }
-    // let dataProps = {
-    //   'userName': userName,
-    //   'password': password
-    // }
-    // axios({
-    //   method: 'post',
-    //   url: servicePath.checkLogin,
-    //   data: dataProps,
-    //   header: { 'Access-Control-Allow-Origin': '*' },
-    //   withCredentials: true
-    // }).then(
-    //   res => {
-    //     console.log(res.data)
-    //     setIsLoading(false)
-    //     if (res.data.data == '登录成功') {
-    //       localStorage.setItem('openId', res.data.openId)
-    //       props.history.push('/index')
-    //     } else {
-    //       message.error('用户名密码错误')
-    //     }
-
-
-    //   }
-    // )
+    if (!userName) {
+      setTimeout(() => {
+        setIsLoading(false)
+        message.error('用户名不能为空')
+      }, 500);
+      return false
+    } else if (!password) {
+      setTimeout(() => {
+        setIsLoading(false)
+        message.error('密码不能为空')
+      }, 500);
+      return false
+    }
+    let dataProps = {
+      'userName': userName,
+      'password': password
+    }
+    axios({
+      method: 'post',
+      url: servicePath.checkLogin,
+      data: dataProps,
+      header: { 'Access-Control-Allow-Origin': '*' },
+      withCredentials: true
+    }).then(
+      res => {
+        console.log(res.data)
+        setIsLoading(false)
+        if (res.data.data === '登录成功') {
+          localStorage.setItem('openId', res.data.openId)
+          props.history.push('/index')
+        } else {
+          message.error('用户名密码错误')
+        }
+      }
+    )
 
     setTimeout(() => {
       setIsLoading(false)
