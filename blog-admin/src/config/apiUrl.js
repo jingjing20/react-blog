d854c0af464880e18a2491334ded44848e1b671b
jingjing20 <1269928993@qq.com>
2020-05-07 22:57:06 +0800

    继续完善

--- blog-admin/src/config/apiUrl.js
+++ blog-admin/src/config/apiUrl.js
@@ -1,6 +1,7 @@
 let ipUrl = 'http://127.0.0.1:7001/admin/'
 
 let servicePath = {
+  getTypeInfo: ipUrl + 'getTypeInfo',  //  获得文章类别信息
   checkLogin: ipUrl + 'checkLogin',  //  检查用户名密码是否正确
 }
 export default servicePath;
